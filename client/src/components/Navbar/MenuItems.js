1e7004b2f4763a97312dfe95045fb1eca81988a2
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-07-12 19:46:31 +0100

    coded and styled elements in navbar

---
+++ client/src/components/Navbar/MenuItems.js
@@ -0,0 +1,17 @@
+export const MenuItems = [
+    {
+        title: 'About Me',
+        url: '#',
+        cName: 'nav-link'
+    },
+    {
+        title: 'Portfolio',
+        url: '#',
+        cName: 'nav-link'
+    },
+    {
+        title: 'Contact Me',
+        url: '#',
+        cName: 'nav-link'
+    }
+]
--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,11 +1,50 @@
 import React from 'react';
 import './style.css';
 
+// import menu-items here as a separate component
+import { MenuItems } from './MenuItems';
+
+// define styles here
+const myStyles = {
+    navbar: {
+        zIndex: '1',
+        position: 'fixed',
+        marginTop: '-8px'
+    }
+}
+
 const Navbar = () => {
     return (
-        <nav>
-            Hi, I'm the navbar!
-        </nav>
+        // Navbar starts here
+        <div className='navbar' style={myStyles.navbar}>
+            <nav>
+                {/* Asad Ansari logo here */}
+                <h1 className='navbar-header'>
+                    <a href='/' className='navbar-logo'>
+                        <b style={{ color:'skyblue' }}>Asad</b>Ansari
+                    </a>
+                </h1>
+
+                {/* Nav-menu and nav-items here */}
+                <div className='menu-icon'>
+
+                </div>
+                <ul>
+                    {MenuItems.map((item, index) => {
+                        return (
+                            <li key={index}>
+                                <a
+                                    className={item.cName}
+                                    href={item.url}
+                                >
+                                    {item.title}
+                                </a>
+                            </li>
+                        )
+                    })}
+                </ul>
+            </nav>
+        </div>
     )
 }
 
