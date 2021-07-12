45a590550cf662dd270ec390d5b580ae2e4e6d72
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-07-12 15:41:09 +0100

    added and styled main landing page

---
+++ client/src/pages/LandingPage.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './style.css';
+
+const LandingPage = () => {
+    return (
+        <div className='landing-page'>
+            <div className='page-header'>
+                Hello, I'm the landing page!
+            </div>
+            <div className='page-header'>
+                Hello, I'm the landing page!
+            </div>
+            <div className='page-header'>
+                Hello, I'm the landing page!
+            </div>
+            <div className='page-header'>
+                Hello, I'm the landing page!
+            </div>
+            <div className='page-header'>
+                Hello, I'm the landing page!
+            </div>
+            <div className='page-header'>
+                Hello, I'm the landing page!
+            </div>
+            <div className='page-header'>
+                Hello, I'm the landing page!
+            </div>
+        </div>
+    )
+}
+
+export default LandingPage;
