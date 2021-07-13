0076bf3a161025b9662b00c399808dba3f228f5c
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-07-13 08:43:34 +0100

    added jsx and image

--- client/src/components/Intro/index.js
+++ client/src/components/Intro/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import './style.css';
 
+// import images here
+import AsadPic from '../../assets/Intro/Asad-Ansari.jpg';
+
 const Intro = () => {
     return (
         <div className='page-intro'>
             <div className='intro-content'>
-                <h1>Hello, this is the page intro!</h1>
+                <h1>Hello, meet Asad Aamir Ansari...</h1>
+                <img
+                    src={AsadPic}
+                    className='img-fluid'
+                >
+
+                </img>
             </div>
         </div>
     )
