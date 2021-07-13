ab188e58fdd83b34b0645a2cca0b11e9c9bf8425
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-07-14 00:17:26 +0100

    added new component and styled it

---
+++ client/src/components/PortfolioItems/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './style.css';
+
+function Portfolio() {
+    return (
+        <div className='portfolio'>
+            <div className='portfolio-container card'>
+                <div className='inner-container card'>
+                    <h1>
+                        Portfolio here.
+                    </h1>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Portfolio;
