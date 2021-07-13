456a91eb5268eada08656940ab8014a763852c9e
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-07-13 23:37:11 +0100

    added in images with their properties, plus other text

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,11 +1,67 @@
-import React from 'react'
+import React from 'react';
+import './style.css';
+
+// import contact icons here
+import Facebook from '../../assets/SocialMediaLinks/facebook.png';
+import Twitter from '../../assets/SocialMediaLinks/twitter.png';
+import Instagram from '../../assets/SocialMediaLinks/instagram.png';
+import LinkedIn from '../../assets/SocialMediaLinks/linkedin.png';
+
+// define styles here
+const myStyles = {
+    icon: {
+        height: '25px'
+    }
+}
 
 const Footer = () => {
     return (
-        <div>
-            <h1>
-                Hi, footer here!
-            </h1>
+        <div className='footer-container'>
+            <footer>
+                <div className='copyright'>
+                    © Copyright Asad Ansari 2021
+                </div>
+                <div className='social-medias'>
+                    <a href='https://www.facebook.com/asadans4ri' target='_blank'>
+                        <img
+                            src={Facebook}
+                            className='img-fluid'
+                            style={myStyles.icon}
+                        >
+                        </img>
+                    </a>
+                    <span>  </span>
+                    <a href='https://www.instagram.com/asadans4ri/' target='_blank'>
+                        <img
+                            src={Instagram}
+                            className='img-fluid'
+                            style={myStyles.icon}
+                        >
+                        </img>
+                    </a>
+                    <span>  </span>
+                    <a href='https://twitter.com/asadans4ri' target='_blank'>
+                        <img
+                            src={Twitter}
+                            className='img-fluid'
+                            style={myStyles.icon}
+                        >
+                        </img>
+                    </a>
+                    <span>  </span>
+                    <a href='https://www.linkedin.com/in/asadansari1/' target='_blank'>
+                        <img
+                            src={LinkedIn}
+                            className='img-fluid'
+                            style={myStyles.icon}
+                        >
+                        </img>
+                    </a>
+                </div>
+                <div className='back-to-top'>
+                    <a>Back To Top</a>
+                </div>
+            </footer>
         </div>
     )
 }
