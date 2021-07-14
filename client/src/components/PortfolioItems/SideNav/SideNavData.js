57de82e4cc03f641fbf2088d64307f5f0fda0ba2
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-07-14 17:50:59 +0100

    styled, adjusted and polished sidenav

---
+++ client/src/components/PortfolioItems/SideNav/SideNavData.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+// import icons here
+import * as FaIcons from 'react-icons/fa';
+
+export const SideNavData = [
+    {
+        title: 'Business',
+        path: '#',
+        icon: <FaIcons.FaBusinessTime />,
+        cName: 'sidenav-text'
+    },
+    {
+        title: 'Philanthropy',
+        path: '#',
+        icon: <FaIcons.FaHandsHelping />,
+        cName: 'sidenav-text'
+    },
+    {
+        title: 'Awards',
+        path: '#',
+        icon: <FaIcons.FaAward />,
+        cName: 'sidenav-text'
+    }
+]
--- client/src/components/PortfolioItems/SideNav/index.js
+++ client/src/components/PortfolioItems/SideNav/index.js
@@ -2,34 +2,43 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './style.css';
 
-// import icons here
-import * as FaIcons from 'react-icons/fa';
-import * as AiIcons from 'react-icons/ai';
+// import sidenav data here
+import { SideNavData } from './SideNavData';
 
 const SideNav = () => {
     // define states here
     const [sidebar, setSidebar] = useState(false);
 
     // define function here to show sidebar when icon is clicked on
-    const showSidebar = () => {
+    const handleClick = () => {
         setSidebar(!sidebar);
     }
 
     return (
-        <div className='sidenav'>
-            <Link to='#' className='menu-bars'>
-                <FaIcons.FaBars
-                    style={{ color: 'cyan' }}
-                    onClick={showSidebar}
-                />
+        <div className={sidebar ? 'sidenav active' : 'sidenav'}>
+            <Link to='#' className='menu-bars' onClick={handleClick}>
+                <i className={sidebar ? 'fas fa-times' : 'fas fa-bars'}></i>
             </Link>
-            <nav className={sidebar ? 'sidenav-menu active' : 'sidenav-manu'}>
-                <ul className='sidenav-menu-items'>
-                    <li className='sidenav-toggle'>
-                        <Link to='#' className='menu-bars'>
-                            <AiIcons.AiOutlineClose />
-                        </Link>
-                    </li>
+            <nav className={sidebar ? 'sidenav-menu active' : 'sidenav-menu'}>
+                <ul className={sidebar ? 'sidenav-menu-items active' : 'sidenav-menu-items'}>
+                    {SideNavData.map((item, index) => {
+                        return (
+                            <li
+                                key={index}
+                                className={item.cName}
+                            >
+                                <Link to={item.path} className='link'>
+                                    <span className='item-icon'>
+                                        {item.icon}
+                                    </span>
+                                    <span> </span>
+                                    <span className='item-title'>
+                                        {item.title}
+                                    </span>
+                                </Link>
+                            </li>
+                        )
+                    })}
                 </ul>
             </nav>
         </div>
