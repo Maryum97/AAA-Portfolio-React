7ab77d9f5cecb235397253bb51b498c92f58a955
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-07-15 08:23:16 +0100

    added animations for text and arrow pointing to sidenav

--- client/src/components/PortfolioItems/PortfolioComponents/Welcome.js
+++ client/src/components/PortfolioItems/PortfolioComponents/Welcome.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
+import { motion } from 'framer-motion';
 import TextTransition, { presets } from "react-text-transition";
 import './styleComponents.css';
+import { inferredPredicate } from '@babel/types';
 
 // define text items here
 const TEXTS = [
@@ -23,8 +25,19 @@ const Welcome = () => {
 
     return (
         <div id='welcome'>
-            <h1 className='welcome-msg'>
+            <motion.div
+                animate={{
+                    opacity: [1, 0.8, 0.6, 0.4, 0.2, 0]
+                }}
+                transition={{
+                    repeat: Infinity
+                }}
+            >
+                <i class="far fa-chevron-double-up"></i>
+            </motion.div>
+            <h1>
                 <TextTransition
+                    className='welcome-msg'
                     text={TEXTS[index % TEXTS.length]}
                     springConfig={presets.molasses}
                 />
