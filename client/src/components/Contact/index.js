a4f9ca2502a1ddc79e93906fcdf43b86da681abb
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-07-15 18:41:51 +0100

    adjusted and made form responsive

--- client/src/components/Contact/index.js
+++ client/src/components/Contact/index.js
@@ -5,7 +5,7 @@ const Contact = () => {
     return (
         <div className='contact'>
             <div className='contact-container'>
-                <h1>Feel free to get in touch with Asad:</h1>
+                <h1 style={{ textAlign: 'center' }}>Feel free to get in touch with Asad:</h1>
                 <br></br>
                 <div className='contact-items card'>
                     <input type='email' className='user-email' placeholder='Your email...'></input>
