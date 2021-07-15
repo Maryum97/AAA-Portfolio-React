32b27672310d6193f4d4b62108ae1743588ab282
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-07-15 18:16:36 +0100

    created and styled contact form

--- client/src/components/Contact/index.js
+++ client/src/components/Contact/index.js
@@ -5,7 +5,19 @@ const Contact = () => {
     return (
         <div className='contact'>
             <div className='contact-container'>
-                <h1>Hi, contact me here!</h1>
+                <h1>Feel free to get in touch with Asad:</h1>
+                <br></br>
+                <div className='contact-items card'>
+                    <input type='email' className='user-email' placeholder='Your email...'></input>
+                    <br></br>
+                    <textarea className='user-message' placeholder='Leave a message...'></textarea>
+                    <br></br>
+                    <button
+                        className='send-btn'
+                    >
+                        Send
+                    </button>
+                </div>
             </div>
         </div>
     )
