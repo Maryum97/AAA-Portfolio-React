58467a96457077fb9f1193dab3afbd1e117bb504
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-07-15 09:06:34 +0100

    edited and played around with css and animations

--- client/src/components/PortfolioItems/PortfolioComponents/Welcome.js
+++ client/src/components/PortfolioItems/PortfolioComponents/Welcome.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 import TextTransition, { presets } from "react-text-transition";
 import './styleComponents.css';
-import { inferredPredicate } from '@babel/types';
 
 // define text items here
 const TEXTS = [
@@ -24,24 +23,27 @@ const Welcome = () => {
     }, []);
 
     return (
-        <div id='welcome'>
+        <div>
             <motion.div
                 animate={{
-                    opacity: [1, 0.8, 0.6, 0.4, 0.2, 0]
+                    opacity: [1, 0, 0]
                 }}
                 transition={{
                     repeat: Infinity
                 }}
             >
-                <i class="far fa-chevron-double-up"></i>
+                <i className="far fa-chevron-double-up"></i>
             </motion.div>
-            <h1>
-                <TextTransition
-                    className='welcome-msg'
-                    text={TEXTS[index % TEXTS.length]}
-                    springConfig={presets.molasses}
-                />
-            </h1>
+            <div id='welcome'>
+                <div></div>
+                <h1>
+                    <TextTransition
+                        className='welcome-msg'
+                        text={TEXTS[index % TEXTS.length]}
+                        springConfig={presets.molasses}
+                    />
+                </h1>
+            </div>
         </div>
     )
 }
