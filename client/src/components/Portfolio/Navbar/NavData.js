b36d61fc1059fbb922b4b590df1d04a636c723fd
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-07-28 11:45:42 +0100

    added data for nav-links

---
+++ client/src/components/Portfolio/Navbar/NavData.js
@@ -0,0 +1,17 @@
+export const NavData = [
+    {
+        title: 'Business',
+        path: '#business',
+        cName: 'port-nav-text'
+    },
+    {
+        title: 'Philanthropy',
+        path: '#philanthropy',
+        cName: 'port-nav-text'
+    },
+    {
+        title: 'Awards',
+        path: '#awards',
+        cName: 'port-nav-text'
+    }
+]
