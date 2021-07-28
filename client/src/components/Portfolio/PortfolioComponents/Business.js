8e286a5c8d649b8b71def3611357b4af5055c129
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-07-28 12:54:32 +0100

    added new files for three different portfolio items

---
+++ client/src/components/Portfolio/PortfolioComponents/Business.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Business = () => {
+    return (
+        <div id='business'>
+            <h1>Business</h1>
+        </div>
+    )
+}
+
+export default Business;
---
+++ client/src/components/Portfolio/PortfolioComponents/Philanthropy.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Philanthropy = () => {
+    return (
+        <div id='philanthropy'>
+            <h1>Philanthropy</h1>
+        </div>
+    )
+}
+
+export default Philanthropy;
