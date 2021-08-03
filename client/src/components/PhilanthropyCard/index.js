2433fe0d698a268503c2ef6d601c3bea862af29a
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-08-03 15:07:08 +0100

    added text and styled and animated content

--- client/src/components/PhilanthropyCard/index.js
+++ client/src/components/PhilanthropyCard/index.js
@@ -1,37 +1,67 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import './style.css';
+
+// aos dependencies
+import Aos from 'aos';
+import 'aos/dist/aos.css';
 
 // import Phil-card data here as a separate component
 import { PhilCardData } from './PhilCardData';
 
 function PhilanthropyCard() {
+    // initialise aos
+    useEffect(() => {
+        Aos.init({ duration: 800 })
+    }, []);
+
     return (
         <div className='phil-card-grid'>
-            <div className='phil-card-container'>
-                <div>
-                    {PhilCardData.map((data, index) => {
-                        return (
-                            <div
-                                className='phil-card'
-                                key={index}
+            {PhilCardData.map((data, index) => {
+                return (
+                    <div
+                        className='phil-card'
+                        key={index}
+                        data-aos='flip-right'
+                        data-aos-easing="ease-in-out"
+                        data-aos-mirror="true"
+                        data-aos-once="false"
+                        data-aos-delay='10'
+                    >
+                        <div>
+                            <img
+                                className='phil-card-logo img-fluid'
+                                src={data.src}
+                                data-aos='zoom-in'
+                                data-aos-easing="ease-in-out"
+                                data-aos-mirror="true"
+                                data-aos-once="false"
+                                data-aos-delay='500'
                             >
-                                <div>
-                                    <img
-                                        className='phil-card-logo img-fluid'
-                                        src={data.src}
-                                    >
-                                    </img>
-                                </div>
-                                <h1 className='phil-card-title'>
-                                    {data.title}
-                                </h1>
-                                <p className='phil-card-description'>
-                                    {data.description}
-                                </p>
-                            </div>
-                        )
-                    })}
-                </div>
-            </div>
+                            </img>
+                        </div>
+                        <h3
+                            className='phil-card-title'
+                            data-aos='flip-down'
+                            data-aos-easing="ease-in-out"
+                            data-aos-mirror="true"
+                            data-aos-once="false"
+                            data-aos-delay='1000'
+                        >
+                            {data.title}
+                        </h3>
+                        <p
+                            className='phil-card-description'
+                            data-aos='flip-down'
+                            data-aos-easing="ease-in-out"
+                            data-aos-mirror="true"
+                            data-aos-once="false"
+                            data-aos-delay='1000'
+                        >
+                            {data.description}
+                        </p>
+                    </div>
+                )
+            })}
         </div>
     )
 }
