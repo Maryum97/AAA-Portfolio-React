3a80ae0209343d4d86a8d285c2277157990be02e
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-08-11 18:47:55 +0100

    added new changes

--- index.js
+++ index.js
@@ -16,6 +16,10 @@ if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
 }
 
+app.use(function (req, res) {
+        res.sendFile(path.join(__dirname, "../client/build/index.html"));
+    });
+
 app.post('/api/forma', (req, res) => {
     let data = req.body;
     let smtpTransport = nodemailer.createTransport({
