ed48df83dde495254e1187b7110904ce9fc8e80d
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-08-11 17:20:02 +0100

    added back-end set-up with express, nodemon, concurrently, cors, nodemailer

--- index.js
+++ index.js
@@ -10,3 +10,56 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
+
+app.post('/api/forma', (req, res) => {
+    let data = req.body;
+    let smtpTransport = nodemailer.createTransport({
+        // service: 'Gmail',
+        // Asad's email, host, pass, on sendinblue
+        host: "smtp-relay.sendinblue.com",
+        port: 587,
+        secure: false,
+        auth: {
+            user: process.env.EMAIL,
+            pass: process.env.PASSWORD
+        }
+    });
+
+    let mailOptions = {
+        from: data.email, // data.email
+        to: `maryum@mayfairitconsultancy.com`,
+        subject: `Message from ${data.name}`,
+        html: `
+        
+        <h3>Information</h3>
+            <ul>
+                <li>Name: ${data.name}</li>
+                <li>Email: ${data.email}</li>
+            </ul>
+
+            <h4>Message:</h4>
+            <p>${data.message}</p>
+
+        `
+    }
+
+    smtpTransport.sendMail(mailOptions, (error, response) => {
+        if (error) {
+            response.send(error);
+            console.log(error);
+        }
+
+        else {
+            response.send('Success!');
+        }
+    })
+
+    smtpTransport.close();
+
+});
+
+const PORT = process.env.PORT || 3005;
+
+app.listen(PORT, () => {
+    console.log(`Server listening at port ${PORT}`);
+});
